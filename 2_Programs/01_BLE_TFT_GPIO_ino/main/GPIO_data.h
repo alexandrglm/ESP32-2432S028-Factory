f7e9d7fca5b20c2cebc1c67dba7567cda48f669a
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2025-05-21 22:42:08 +0200

    Create GPIO_data.h

---
+++ 2_Programs/01_BLE_TFT_GPIO_ino/main/GPIO_data.h
@@ -0,0 +1,13 @@
+#ifndef GPIO_DATA_H
+#define GPIO_DATA_H
+
+/*
+ * GPIO TRIGGERING:
+ *
+ * LED triggers for this specific board:
+ * 
+ * 16: GREEN / 17: BLUE / 18: RED
+ */
+#define LED_PIN 16
+
+#endif
