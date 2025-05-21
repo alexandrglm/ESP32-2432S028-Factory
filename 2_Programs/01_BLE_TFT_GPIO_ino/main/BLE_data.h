6c5acad27f2d666d3cfcba35b0287754163b05e5
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2025-05-21 22:39:43 +0200

    Create BLE_data.h

---
+++ 2_Programs/01_BLE_TFT_GPIO_ino/main/BLE_data.h
@@ -0,0 +1,14 @@
+#ifndef BLE_DATA_H
+#define BLE_DATA_H
+/*
+ * SERVER NAME -> 
+ * LED SERVIDE UUID / CHARASTERICIS UUID -> Use uuifgen
+ */
+
+#define bleServerName "ESP32_TEST_SERVER"
+
+#define LED_SERVICE_UUID "5aead110-d619-47b0-86f9-6bedb76f7ea1"
+
+#define LED_CHARACTERISTIC_UUID "5ccb8cbe-1601-40f5-b367-f62e5efc3460"
+
+#endif
